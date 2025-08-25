7bf6dbeec854edbf2194e7fdfae81145176df788
Paulo-Galego <paulosilvanogalego@gmail.com>
2025-08-25 10:11:27 -0300

    Add project files.

---
+++ Entities/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorAppTarefasMacoratti.Entities
+{
+	public class Tarefa
+	{
+		public Guid ID { get; set; }
+		public string Descricao { get; set; }
+		public bool Concluida { get; set; }
+		public DateTime DataCriacao { get; set; }
+	}
+}
